43d308a2945eed3e99ac7194d1956853ae6984d8
emirmcgreevy <emir@umich.edu>
2024-11-10 21:54:16 -0500

    commit400

--- csv_to_athletes_html.py
+++ csv_to_athletes_html.py
@@ -66,9 +66,9 @@ def gen_athlete_page(data, outfile):
         </h1>
         <nav>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="mens_roster.html">Men's Team</a></li>
-                <li><a href="womens_roster.html">Women's Team</a></li>
+                <li><a href="/index.html">Home</a></li>
+                <li><a href="/mens_roster.html">Men's Team</a></li>
+                <li><a href="/womens_roster.html">Women's Team</a></li>
             </ul>
         </nav>
     </header>
